29c9900098a5452b1a8b8e0ce68546eec6f20b1d
hwiy01 <vdocom02@naver.com>
2025-05-02 16:21:11 +0900

    init: 메인 화면 구현

---
+++ week3-gitSearch-baseballGame/src/App.jsx
@@ -0,0 +1,79 @@
+/** @jsxImportSource @emotion/react */
+import { useState } from 'react'
+import styled from '@emotion/styled'
+import GitSearch from './pages/gitSearch'
+import { BaseballGame } from './pages/BaseballGame'
+
+
+function App() {
+  const [page, setPage] = useState('gitSearch')
+
+  return (
+    <Container>
+      <Header>
+        <Title>🐢 깃허브 검색 &nbsp; | &nbsp; 숫자 야구 게임 🐰</Title>
+        <Nav>
+          <Button onClick={() => setPage('gitSearch')}> 🐈‍⬛</Button>
+          <Button onClick={() => setPage('baseballGame')}>⚾️</Button>
+        </Nav>
+      </Header>
+      <Main>
+        {page === 'gitSearch' && <GitSearch />}
+        {page === 'baseballGame' && <BaseballGame />}
+      </Main>
+    </Container>
+  )
+}
+
+export default App
+
+const Container = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  width: 100vw;
+  min-height: 100vh;
+  display: flex;
+  flex-direction: column;
+`
+
+const Header = styled.header`
+  text-align: center;
+  margin-bottom: 16px;
+  background-color: aliceblue;
+  width: 100%;
+  padding: 50px;
+`
+
+const Title = styled.div`
+  font-size: 2rem;
+  font-weight: bold;
+  margin-bottom: 12px;
+`
+
+const Nav = styled.nav`
+  display: flex;
+  justify-content: center;
+  gap: 16px;
+  padding: 10px;
+`
+
+const Button = styled.button`
+  padding: 10px 20px;
+  border: none;
+  background-color: #ececec;
+  color: #e3e3e3;
+  font-size: 16px;
+  border-radius: 8px;
+  cursor: pointer;
+  transition: background-color 0.3s;
+  border: 2px solid #55555556;
+
+  &:hover {
+    background-color: #555;
+  }
+`
+
+const Main = styled.main`
+  margin-top: 32px;
+`
