3c58c8ba3c145bbee1cb784d889a9c43ed227b4d
hwiy01 <vdocom02@naver.com>
2025-05-11 15:41:23 +0900

    feat: 마이페이지 닉네임 수정 기능

--- week4-login/src/apis/auth.ts
+++ week4-login/src/apis/auth.ts
@@ -27,3 +27,38 @@ export const fetchSignUp = async (loginId: string, password: string, nickname: s
         throw new Error(message);
     }
 }
+
+export const fetchMypage = async () => {
+    try {
+        const userId = localStorage.getItem('id');
+        const response = await axiosInstance.get('/api/v1/users/me', 
+          {  
+            headers: {
+            userId: userId,
+        }});
+        return response.data;
+    }catch (e: any) {
+        // 서버 에러 메시지를 추출해서 다시 던짐
+        const message = e.response?.data?.message || '알 수 없는 오류입니다.';
+        throw new Error(message);
+    }
+}
+
+export const patchNickname = async (newNickname: string) => {
+    const userId = localStorage.getItem('id');
+    try {
+        const response = await axiosInstance.patch(
+            '/api/v1/users',
+            { nickname: newNickname },
+            {
+                headers: {
+                    userId: userId,
+                },
+            }
+        );
+        return response.data;
+    } catch (e: any) {
+        const message = e.response?.data?.message || '알 수 없는 오류입니다.';
+        throw new Error(message);
+    }
+};
--- week4-login/src/components/Header.tsx
+++ week4-login/src/components/Header.tsx
@@ -1,7 +1,47 @@
-import React from 'react'
+import styled from '@emotion/styled';
+import React from 'react';
 
-export const Header = () => {
+interface HeaderProps{
+  userName: string;
+}
+
+export const Header = ({userName} : HeaderProps) => {
   return (
-    <div>Header</div>
+    <>
+      <Container>
+        <Menu>
+          <Button>내정보</Button>
+          <Button>SOPT회원 조회하기</Button>
+          <Button>로그아웃</Button>
+        </Menu>
+        <Menu>
+          <Button>🧚 {userName}</Button>
+        </Menu>
+      </Container>
+    </>
   )
 }
+
+const Container = styled.div`
+  width: 100%;
+  background-color: aliceblue;
+  height: 80px;
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  padding: 20px;
+  box-sizing: border-box; 
+`
+
+const Menu = styled.div`
+  display: flex;
+  flex-direction: row;
+  gap: 18px;
+`
+
+const Button = styled.div`
+  font-size: 15px;
+  cursor: pointer;
+  display: flex;
+  flex-direction: row;
+`
